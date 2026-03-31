178d469998a2956bf2908a4337bb34824f639398
Kenny Romero <sastoque.kenny@gmail.com>
2026-03-31 11:47:09 -0500

    fix: solve bug with painter placeholder

--- app/src/main/java/com/kenny/rickandmortyapp/presentation/components/CharacterItem.kt
+++ app/src/main/java/com/kenny/rickandmortyapp/presentation/components/CharacterItem.kt
@@ -18,13 +18,12 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.ColorPainter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
-import com.kenny.rickandmortyapp.R
 import com.kenny.rickandmortyapp.domain.model.Character
 import com.kenny.rickandmortyapp.domain.model.CharacterStatus
 
@@ -44,8 +43,8 @@ fun CharacterItem(character: Character) {
                     .crossfade(true)
                     .build(),
                 contentDescription = character.name,
-                placeholder = painterResource(R.drawable.placeholder),
-                error = painterResource(R.drawable.error_image),
+                placeholder = ColorPainter(Color(0xFFCCCCCC)),
+                error = ColorPainter(Color(0xFFFFCCCC)),
                 contentScale = ContentScale.Crop,
                 modifier = Modifier
                     .size(80.dp)
